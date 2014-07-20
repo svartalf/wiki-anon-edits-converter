091921af6942eae792b837fa461f85e8459576b0
svartalf <self@svartalf.info>
2014-07-20 23:38:34 +0900

    Flushing output file after an each row. For testing with csv format

--- output.c
+++ output.c
@@ -10,6 +10,7 @@ void output_write_header(FILE *f) {
 
 void output_write_row(FILE *f, Revision *rev) {
 	char *output;
-	sprintf(output, "%d,%d,%d,%lu,%lu", rev->page_id, rev->old_id, rev->new_id, rev->ip, rev->timestamp);
+	sprintf(output, "%d,%d,%d,%lu,%lu\n", rev->page_id, rev->old_id, rev->new_id, rev->ip, rev->timestamp);
 	fwrite(output, strlen(output), 1, f);
+	fflush(f);
 }
